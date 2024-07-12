5873d722989e0c27b787733811bcd2f4f706e3d0
RonexLemon <ronexondimu@gmail.com>
2024-07-13 01:12:21 +0300

    generate

--- packages/react-app/components/pages/generateLink.tsx
+++ packages/react-app/components/pages/generateLink.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Button } from "@/components/ui/button";
 import {
   Card,
@@ -11,12 +11,73 @@ import {
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { QrCode } from "lucide-react";
 import { QRcode } from "../qrcode";
+import useContract from "@/contracts/useContracts";
+import { ethers } from "ethers";
+import { useWaitForTransactionReceipt } from "wagmi";
+import { EASYPAYCONTRACT } from "@/constants/constant";
+import EASYABI from "./../../abi/EASYPAY.json";
 
 export function GenerateCard() {
   const [copied, setCopied] = useState(false);
-  const [opendrawer,setOpenDrawer] = useState<boolean>(false)
+  const [opendrawer, setOpenDrawer] = useState<boolean>(false);
+  const [amount, setAmount] = useState<bigint | undefined>();
+  const [code, setCode] = useState<`0x${string}`>();
+  const [codenew, setCodeNew] = useState<any>();
+  const { approve, generateCode, claimCode } = useContract();
+
+  const handleClaim =async()=>{
+    try{
+      //https://easypay/0x6c2870113da898f5eb511ffcd6e9dd6ddd9c698b981050fa435fe043bb21d5fa
+      const tx = await claimCode("0x6c2870113da898f5eb511ffcd6e9dd6ddd9c698b981050fa435fe043bb21d5fa");
+
+    }catch(err){
+      console.log(err)
+    }
+  }
+
+  const result = useWaitForTransactionReceipt({
+    hash: code,
+    
+  });
+
+  useEffect(() => {
+    if (result.data) {
+      console.log("Transaction receipt received:", result.data);
+
+      const iface = new ethers.Interface(EASYABI.abi);
+
+      const parsedLogs = result.data.logs.map(log => {
+        return iface.parseLog({ data: log.data, topics: log.topics });
+    })
+    console.log("parsefaces",parsedLogs[2]?.args[1]);
+    // console.log("all parse",parsedLogs);
+    // console.log("parsefaces name",parsedLogs[2]?.name);
+    // console.log("all parse topics",parsedLogs[2]?.topic);
+    // console.log("all parse topics 1",parsedLogs[1]?.topic);
+    setCodeNew(parsedLogs[2]?.args[1])
+    } else if (result.error) {
+      console.error("Error waiting for transaction receipt:", result.error);
+    }
+  }, [result.data, result.error]);
+
+  const handleApproveAndGenerate = async () => {
+    try {
+
+      const appr = await approve(amount);
+      console.log("approve:", appr);
+
+      if (appr) {
+        const data = await generateCode(amount);
+        setCode(data);
+        console.log("The code is generated:", data);
+      } else {
+        console.log("Approval failed.");
+      }
+    } catch (err) {
+      console.error("Error in handleApproveAndGenerate:", err);
+    }
+  };
 
   const copyToClipboard = () => {
     const textToCopy = `https://easypay/32673656476375373756357357357357`;
@@ -27,38 +88,48 @@ export function GenerateCard() {
 
   return (
     <div>
-     
-<Card className="w-[400px] bg-gray-300">
-      <CardHeader>
-        <CardTitle>EASYPAY</CardTitle>
-        <CardDescription>Deploy your new project in one-click.</CardDescription>
-      </CardHeader>
-      <CardContent>
-        <div className="grid w-full items-center gap-4">
-          <div className="flex flex-col space-y-1.5">
-            <Label htmlFor="name">Amount</Label>
-            <Input id="name" placeholder="$1" />
-          </div>
-          <div className="flex flex-col items-center gap-2">
-            <div className="text-sm"> 
-                
-            <Label className="text-sm" >{`https://easypay/32673656476375373756357357357357`}</Label>
-
+      <Card className="w-[400px] bg-gray-300">
+        <CardHeader>
+          <CardTitle>EASYPAY</CardTitle>
+          <CardDescription>Deploy your new project in one-click.</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <div className="grid w-full items-center gap-4">
+            <div className="flex flex-col space-y-1.5">
+              <Label htmlFor="amount">Amount</Label>
+              <Input
+                id="amount"
+                onChange={(e) => setAmount(ethers.parseEther(e.target.value))}
+                placeholder="$1"
+              />
             </div>
-            <Button onClick={()=>setOpenDrawer(true)}>Open</Button>
-           
-            <Button onClick={copyToClipboard} className="bg-blue-500 text-white px-3 py-1 rounded-md">
-              {copied ? "Copied!" : "Copy"}
-            </Button>
+            <div className="flex flex-col items-center gap-2">
+              {codenew && (
+                <Button onClick={() => setOpenDrawer(true)}>Get Code</Button>
+              )}
+            </div>
           </div>
-        </div>
-      </CardContent>
-      <CardFooter className="flex">
-        <Button className="w-full bg-green-300 rounded-2xl">Generate</Button>
-      </CardFooter>
-    </Card>
-    <QRcode link="https://easypay/32673656476375373756357357357357"  state={opendrawer} setState={setOpenDrawer}/>
+        </CardContent>
+        <CardFooter className="flex flex-col">
+          <Button
+            onClick={handleApproveAndGenerate}
+            className="w-full bg-green-300 rounded-2xl"
+          >
+            Generate
+          </Button>
+          <Button
+            onClick={handleClaim}
+            className="w-full bg-green-300 rounded-2xl"
+          >
+          Claim
+          </Button>
+        </CardFooter>
+      </Card>
+      <QRcode
+        link={`https://easypay/${codenew}`}
+        state={opendrawer}
+        setState={setOpenDrawer}
+      />
     </div>
-    
   );
 }
---
+++ packages/react-app/contracts/useContracts.tsx
@@ -0,0 +1,65 @@
+import React from "react";
+
+import { useWriteContract,useReadContract,useWaitForTransactionReceipt } from "wagmi";
+import { useAccount } from "wagmi";
+import { EASYPAYCONTRACT } from "@/constants/constant";
+import EASYABI from "../abi/EASYPAY.json";
+import CUSDABI from "../abi/IERC20.json";
+import { CUSDCONTRACT } from "@/constants/constant";
+
+
+const useContract =()=>{
+
+    const {writeContractAsync: easycontract} = useWriteContract();
+    const {writeContractAsync: cusdcontract} = useWriteContract();
+    const {data} = useWaitForTransactionReceipt();
+
+    const generateCode = async(amount:bigint |undefined)=>{
+        const data = await easycontract({
+            address: EASYPAYCONTRACT,
+            functionName: "generateLink",
+            args: [amount],
+            abi: EASYABI.abi,
+            });
+
+            // const result = useWaitForTransactionReceipt({
+            //     hash: data,
+            //   })
+            // return result.data?.logs[0].topics[2];
+            return data
+
+    }
+
+    const claimCode = async(code:string)=>{
+        const data = await easycontract({
+            address: EASYPAYCONTRACT,
+            functionName: "claim",
+            args: [code],
+            abi: EASYABI.abi,
+            });
+
+            return data;
+
+    }
+
+const approve = async(amount:bigint | undefined)=>{
+    const val = await cusdcontract({
+        address: CUSDCONTRACT,
+        functionName: "approve",
+        args: [EASYPAYCONTRACT,amount],
+        abi: CUSDABI.abi,
+    })
+    // const result = useWaitForTransactionReceipt({
+    //     hash: val,
+    //   })
+    // return result.data?.logs[0].topics[2];
+    return val
+    
+}
+
+return{
+    approve,claimCode,generateCode
+    }
+
+}
+export default useContract
